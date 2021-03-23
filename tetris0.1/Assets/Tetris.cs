b807885ebf658fb1740beb6c1e2ff3a64c72e86b
DeathWing27 <78451824+DeathWing27@users.noreply.github.com>
2021-03-23 13:21:45 +0200

    4
    FourGame

---
+++ tetris0.1/Assets/Tetris.cs
@@ -0,0 +1,189 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tetris : MonoBehaviour {
+	int xCord=3;
+	int yCord=1;
+
+    //16x8
+    public int[,] pole = new int[,]{
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,1,1,1,0,0},
+        {0,0,0,0,1,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {2,0,0,0,0,0,0,2},
+        {2,0,0,0,0,0,0,2},
+        {2,0,0,0,0,0,0,2},
+        {2,2,2,2,2,2,2,2},
+    };
+	
+   
+	void Start () {
+    				
+
+	}
+
+    //двигаемся вправо
+	void MoveRight(){
+		int[,] tmp = new int[16,8];
+		for (int y = 0; y < 16; y++) {
+			for (int x = 0; x < 8; x++) {
+				if ( x == 7 && pole[y,x] == 1){ 
+					return;
+				}
+				if ( pole[y,x] == 1 && pole[y,x+1] == 2 ){
+					return;
+				}
+
+				if ( pole[y,x] == 1 ){
+					tmp[y,x+1] = 1;
+				}
+				if ( pole[y,x] == 2 ){
+					tmp[y,x] = pole[y,x];
+				}
+			}
+		}
+		xCord++;
+		pole = tmp;
+		
+	}
+
+    //движение влево
+	void MoveLeft(){
+		
+		int[,] tmp = new int[16,8];
+		for (int y = 0; y < 16; y++) {
+			for (int x = 7; x >= 0; x--) {
+				if ( x == 0 && pole[y,x] == 1){ 
+					return;
+				}
+				if ( pole[y,x]==1 && pole[y,x-1] == 2 ){
+					return;
+				}
+				if ( pole[y,x] == 1 ){
+					tmp[y,x-1] = 1;
+				}
+
+				if ( pole[y,x] == 2 ){
+					tmp[y,x] = pole[y,x];
+				}
+			}
+		}
+		xCord--;
+		pole = tmp;
+		
+	}
+
+    //движение вниз
+	void MoveDown(){
+		
+		int[,] tmp = new int[16,8];
+
+		for (int y = 15; y >= 0; y--) {
+			for (int x = 0; x < 8; x++) {
+				if (y > 0) {
+					if (pole [y, x] == 2 && pole [y - 1, x] == 1) {
+						Replace (); //заменяем все единицы на двойки
+						return; //преккащаем
+					}
+				}
+				if (y == 15 && pole [y, x] == 1) { //добрались до низа  
+					Replace (); //заменяем все единицы на двойки
+					return; //преккащаем
+				}   
+
+				if (y < 15) {
+					if (pole [y, x] == 1) {
+						tmp [y + 1, x] = 1;
+					}
+				}
+
+				if (pole [y, x] == 2) {
+					tmp [y, x] = 2;
+				}
+			}
+		}
+
+
+		yCord++;
+		pole = tmp;
+
+
+
+	}
+	void Replace () {
+		for (int y = 0; y < 16; y++) {
+			for (int x = 0; x < 8; x++) {    
+				if ( pole[y,x] >0 ){ //если не 0
+					pole[y,x] = 2; //записываем 2
+				}
+			}
+		}
+
+
+	}
+	void Rotate () {
+		
+		int[,] tmp = new int[3, 3];
+	
+		for (int y = yCord; y < yCord + 3; y++) {
+			for (int x = xCord; x < xCord + 3; x++) {
+				tmp [y - yCord, x - xCord] = pole [y, x];
+				if (pole [y, x] == 2) {
+					return;
+				}
+			}
+		}
+		for (int y = 0; y < 3; y++) {
+			for (int x = 0; x < 3; x++) {
+				pole [y + yCord, x + xCord] = tmp [2 - x, y];
+			}
+		}
+	}
+	void CleanLine( int line )
+		{
+			for (int x=0; x<8; x++) {
+				pole[line,x] = 0;
+			}
+		}
+
+
+
+
+
+					
+
+
+
+	public virtual void Update(){	
+
+		if ( Input.GetKeyDown("s")){
+			MoveDown();
+		}
+
+		if ( Input.GetKeyDown("d")){
+			MoveRight();
+		}
+
+		if ( Input.GetKeyDown("a")){
+			MoveLeft();
+		}
+		if (Input.GetKeyDown("space")) {
+			Rotate ();
+		}
+		//if (Input.GetKeyDown("c")) {
+		//	CleanLine (); 
+		//}
+		   		
+	}
+}
+
+
---
+++ tetris0.1/Assets/UITetris.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITetris : MonoBehaviour {
+
+    public GameObject blockPfb;
+    public GameObject[,] blocks;
+    Tetris tet;
+
+	// Use this for initialization
+	void Start () {
+		
+        tet = this.GetComponent<Tetris>();
+
+        blocks = new GameObject[16,8];
+
+        for (int y = 0; y < 16; y++) {
+            for (int x = 0; x < 8; x++) {
+                blocks[y,x] = GameObject.Instantiate( blockPfb );
+                blocks[y,x].transform.position = new Vector3( x, 15-y, 0 );
+            }
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        for (int y = 0; y < 16; y++) {
+            for (int x = 0; x < 8; x++) {
+                if ( tet.pole[y,x] > 0 ){
+                    blocks[y,x].SetActive( true );
+                    if ( tet.pole[y,x] == 1 ){
+                        blocks[y,x].GetComponent<SpriteRenderer>().color = Color.red;
+                    }
+                    else
+                    {
+                        blocks[y,x].GetComponent<SpriteRenderer>().color = Color.white;    
+                    }
+
+                }
+                else{
+                    blocks[y,x].SetActive( false );
+                    blocks[y,x].GetComponent<SpriteRenderer>().color = Color.white;
+                }
+            }
+        }
+
+	}
+
+}
